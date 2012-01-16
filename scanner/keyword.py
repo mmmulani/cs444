0ab832d4aba233383a7a4da35d3f07a1bd8be66c
gnleece <gnleece@student.cs.uwaterloo.ca>
2012-01-15 23:06:41 -0500

    Add Keyword DFA

---
+++ scanner/keyword.py
@@ -0,0 +1,61 @@
+from components.composed_dfa import ComposedDFA
+from components.one_of import OneOf
+from components.string_dfa import String
+
+class Keyword(ComposedDFA):
+
+  KEYWORDS = [    
+      "abstract",
+      "boolean",
+      "break",
+      "byte",
+      "case",
+      "catch",
+      "char",
+      "class",
+      "const",
+      "continue",
+      "default",
+      "do",
+      "double",
+      "else",
+      "extends",
+      "final",
+      "finally",
+      "float",
+      "for",
+      "goto",
+      "if",
+      "implements",
+      "import",
+      "instanceof",
+      "int",
+      "interface",
+      "long",
+      "native",
+      "new",
+      "package",
+      "private",
+      "protected",
+      "public",
+      "return",
+      "short",
+      "static",
+      "strictfp",
+      "super",
+      "switch",
+      "synchronized",
+      "this",
+      "throw",
+      "throws",
+      "transient",
+      "try",
+      "void",
+      "volatile",
+      "while",
+  ]
+
+  def __init__(self):
+    machine_list = [String(x) for x in Keyword.KEYWORDS]
+    self.machine = OneOf(*machine_list)
+    super(Keyword, self).__init__()
