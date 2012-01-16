2f84719f5572abef7e00d0aa574866e434f7e6ca
Andrew Song <song.exe@gmail.com>
2012-01-16 00:25:36 -0500

    We don't need to import itertools in operator anymore.

--- scanner/operator.py
+++ scanner/operator.py
@@ -1,7 +1,5 @@
 from components import string_dfa, composed_dfa, concat, one_of
 
-import itertools
-
 class Operator(composed_dfa.ComposedDFA):
   def __init__(self):
     ops = ['=', '>', '<', '!', '~', '?', ':', '+', '-', '*', '/', '&', '|',
