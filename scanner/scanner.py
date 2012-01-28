e248d82e5be84b9fba3f47ee8f55fe1e86202d53
Andrew Song <song.exe@gmail.com>
2012-01-28 15:52:51 -0500

    Fix a bug in the scanner with single-character tokens ending the string.
    Also:
      => Move token map out of scanner class into token class
      => + tests
      => Refactor scanner to be eaiser to read.

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -16,6 +16,16 @@ class Token:
   SEPARATOR = 'SEPARATOR'
   OPERATOR = 'OPERATOR'
 
+  token_map = {
+      operator.Operator: OPERATOR,
+      separator.Separator: SEPARATOR,
+      literal.Literal: LITERAL,
+      keyword.Keyword: KEYWORD,
+      identifier.Identifier: IDENTIFIER,
+      comment.Comment: COMMENT,
+      whitespace.Whitespace: WHITESPACE
+  }
+
 class Scanner(object):
   '''Scanner object
   Responsible for scanning/tokenizing valid JOS programs given as a string'''
@@ -30,16 +40,6 @@ class Scanner(object):
       comment.Comment(),
       whitespace.Whitespace()]
 
-  token_map = {
-      operator.Operator: Token.OPERATOR,
-      separator.Separator: Token.SEPARATOR,
-      literal.Literal: Token.LITERAL,
-      keyword.Keyword: Token.KEYWORD,
-      identifier.Identifier: Token.IDENTIFIER,
-      comment.Comment: Token.COMMENT,
-      whitespace.Whitespace: Token.WHITESPACE
-  }
-
   @staticmethod
   def get_token_list(s):
     s = Scanner(s)
@@ -53,26 +53,33 @@ class Scanner(object):
     i = 0
 
     while i < len(self.s):
-      cur_machines = [m.clone() for m in Scanner.machines]
-      last_final = None
-      while len(cur_machines) > 0 and i < len(self.s):
-        c = self.s[i]
-        cur_machines = [m for m in cur_machines if m.delta(c)]
-        final_tokens = [ix for ix, m in enumerate(cur_machines) if m.is_final()]
-        if len(final_tokens) > 0:
-          # Keep track of the final token with the highest priority.
-          last_final = self._get_token_from_machine(
-              cur_machines[final_tokens[0]])
-        i += 1
-
-      if last_final:
-        # No more valid machines.
-        yield last_final
-        if i < len(self.s):
-          i -= 1
-      else:
+      tok = self._get_next_token(i)
+      if tok is None:
         # Scanning error.
-        raise Exception('Scanning error: {0}', self.s[i:])
+        raise Exception('Scanning error: <{0}>'.format(self.s[i:]))
+      yield tok
+
+      i += len(tok[1])
+
+  def _get_next_token(self, i):
+    '''Get the next token start from position i of the string'''
+    cur_machines = [m.clone() for m in Scanner.machines]
+    last_final = None
+    l = 0
+
+    for c in self.s[i:]:
+      cur_machines = [m for m in cur_machines if m.delta(c)]
+      final_token_ix = [ix for ix, m in enumerate(cur_machines)
+          if m.is_final()]
+      if len(final_token_ix) > 0:
+        # Keep track of the final token with the highest precedence.
+        last_final = self._get_token_from_machine(
+            cur_machines[final_token_ix[0]])
+
+      if len(cur_machines) == 0:
+        break
+
+    return last_final
 
   def _get_token_from_machine(self, m):
-    return (Scanner.token_map[type(m)], m.lexeme)
+    return (Token.token_map[type(m)], m.lexeme)
--- scanner/tests/scanner_test.py
+++ scanner/tests/scanner_test.py
@@ -4,9 +4,27 @@ from ..scanner import Scanner, Token
 
 class TestScanner(unittest.TestCase):
   def test_simple_tokens(self):
+    '''Test simple strings of tokens'''
     toks = Scanner.get_token_list('test test')
     self.assertListEqual([
         (Token.IDENTIFIER, 'test'),
         (Token.WHITESPACE, ' '),
         (Token.IDENTIFIER, 'test')],
+        toks)
+
+  def test_single_char_token(self):
+    '''Test combination of single-character tokens'''
+    toks = Scanner.get_token_list(' ')
+    self.assertListEqual([(Token.WHITESPACE, ' ')], toks)
+
+    toks = Scanner.get_token_list('t ')
+    self.assertListEqual([
+        (Token.IDENTIFIER, 't'),
+        (Token.WHITESPACE, ' ')],
+        toks)
+
+    toks = Scanner.get_token_list('  ')
+    self.assertListEqual([
+        (Token.WHITESPACE, ' '),
+        (Token.WHITESPACE, ' ')],
         toks)
