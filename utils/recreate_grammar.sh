f060557cab3eff7352b01318d562d5cf2d4561bb
Mehdi Mulani <mmmulani@uwaterloo.ca>
2012-02-07 18:36:13 -0500

    Changed to a LALR1 grammar. (33% faster)

--- utils/recreate_grammar.sh
+++ utils/recreate_grammar.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 python check_symbols.py
-python create_cfg.py > test.cfg && java jlalr.Jlr1 < test.cfg > joos.lr1 && python build_shift_reduce_maps.py joos.lr1 joos_reduce.txt joos_shift.txt
+python create_cfg.py > test.cfg && java jlalr.Jlalr1 < test.cfg > joos.lr1 && python build_shift_reduce_maps.py joos.lr1 joos_reduce.txt joos_shift.txt
