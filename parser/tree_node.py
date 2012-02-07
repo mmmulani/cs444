cf7ed516b956a500e09b6aa5e2c1faf977b2e309
Gwynneth Leece <gleece@gmail.com>
2012-02-06 23:40:44 -0500

    Sexy pretty-oprinting for trees!

--- parser/tree_node.py
+++ parser/tree_node.py
@@ -6,7 +6,9 @@ class TreeNode():
   def set_children(self, children):
     self.children = children
 
-  def debug_print(self, depth = 0):
-    print("\t"*depth + str(self.value))
-    for child in self.children:
-      child.debug_print(depth+1)
+  def debug_print(self, prefix = ""):
+    print prefix[:-8] + "|------ " + str(self.value)
+    for i in range(0, len(self.children)-1):
+      self.children[i].debug_print(prefix + "|       ")
+    if len(self.children) > 0:
+      self.children[-1].debug_print(prefix + "        ")
