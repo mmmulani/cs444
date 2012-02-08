8fa64abde18cc5ef57463d867fda800bb79a36fb
Andrew Song <song.exe@gmail.com>
2012-02-08 00:27:02 -0500

    Fix Je_Native Native methods aren't allowed unless they're static, return int, and take one int param.

--- weeder/weeder.py
+++ weeder/weeder.py
@@ -8,7 +8,7 @@ class Weeder(object):
   Takes a parser tree, and validates it.
   If an error is found, an exception is thrown
   '''
-  
+
   valid_modifiers = {
     'ClassDeclaration' : set(['public', 'protected', 'private', 'abstract',
                               'static', 'final']),
@@ -53,17 +53,25 @@ class Weeder(object):
             raise WeedingError('Class cannot be both abstract and final')
         elif tree.value == 'MethodHeader':
           no_abstract = set(['private', 'static', 'final', 'native'])
-          if (('abstract' in modifiers_set and 
+          if (('abstract' in modifiers_set and
                len(modifiers_set.intersection(no_abstract)) > 0) or
               ('static' in modifiers_set and 'final' in modifiers_set)) :
             raise WeedingError('MethodHeader has invalid modifiers')
 
+        # No native methods except for static native int.
+        if tree.value == 'MethodHeader' and 'native' in modifiers_set:
+          if 'static' in modifiers_set and tree.children[1].value == 'Type':
+            if self._get_type_from_node(tree.children[1]) == 'int':
+              # This is the only valid case.
+              pass
+          raise WeedingError('No native methods except static native int')
+
       else:
         self._verify_modifiers(child)
-    
+
     if tree.value == 'MethodHeader' and not(has_modifiers):
       raise WeedingError('Methods must have a modifier')
-  
+
   def _get_modifiers_list(self, tree):
     if tree.value == 'Modifier':
       # the node should have one child, which is the actual modifier keyword
@@ -73,4 +81,12 @@ class Weeder(object):
       for child in tree.children:
         modifiers = modifiers + self._get_modifiers_list(child)
       return modifiers
-  
+
+  def _get_type_from_node(self, tree):
+    if len(tree.children) == 0:
+      return tree.value
+    elif tree.value == 'Identifiers':
+      # TODO: Implement this if you need it
+      raise Exception('Need to implement getting type from Identifiers node')
+    else:
+      return self._get_type_from_node(tree.children[0])
