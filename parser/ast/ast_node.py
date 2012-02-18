7b3d4471d3f6a9fcb91aeccfca85529e01cbe1f6
Andrew Song <song.exe@gmail.com>
2012-02-18 16:44:22 -0500

    Create an ASTUtils namespace class

--- parser/ast/ast_node.py
+++ parser/ast/ast_node.py
@@ -11,3 +11,19 @@ class ASTNode(object):
 
   def _show(self):
     print type(self).__name__
+
+class ASTUtils():
+  '''Namespace class for AST utilities'''
+
+  @staticmethod
+  def get_ids_list(tree):
+    '''Given an identifiers node, returns a flattened list of identifiers'''
+    # Identifiers Identifier
+    # Identifiers Identifers . Identifer
+    ret = []
+    while len(tree.children) == 3:
+      ret.append(tree.children[2].lexeme)
+      tree = tree.children[0]
+    ret.append(tree.children[0].lexeme)
+    ret.reverse()
+    return ret
--- parser/ast/ast_type.py
+++ parser/ast/ast_type.py
@@ -13,24 +13,11 @@ class ASTType(ast_node.ASTNode):
     if len(tree.children) == 0:
       return tree.value
     elif tree.value == 'Identifiers':
-      return self.get_identifiers_type(tree)
+      return ast_node.ASTUtils.get_ids_list(tree)
     elif tree.value == 'ArrayType':
       self.is_array = True
 
     return self.get_type_from_node(tree.children[0])
-
-  def get_identifiers_type(self, tree):
-    '''Returns a list of identifiers given an Identifiers node.'''
-
-    # This code is so beautiful. Thanks, Mehdi!
-    ids = []
-    while len(tree.children) == 3:
-      ids.append(tree.children[2].lexeme)
-      tree = tree.children[0]
-
-    ids.append(tree.children[0].lexeme)
-    ids.reverse()
-    return ids
 
   def is_primitive(self):
     return (type(self.children[0]) != type([]))
