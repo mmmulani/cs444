969dc01d44afe2bd229958c5b9af64d9790b9aa7
Andrew Song <song.exe@gmail.com>
2012-02-27 18:13:27 -0500

    Start with a more simple env base class

---
+++ name_resolution/env.py
@@ -0,0 +1,29 @@
+class Environment(object):
+  '''The base environment object'''
+  def __init__(self, parent):
+    self.parent = parent
+
+  def lookup_method(self, sig):
+    # Overload this if you need to look up methods in your context.
+    raise EnvironmentError('lookup_method() not overloaded.')
+
+  def lookup_field(self, name):
+    # Overload this if you need to look up fields in your context.
+    raise EnvironmentError('lookup_field() not overloaded.')
+
+  def lookup_formal(self, name):
+    # Overload this if you need to look up formal params in your context.
+    raise EnvironmentError('lookup_formal() not overloaded.')
+
+  def lookup_local(self, name):
+    # Overload this if you need to look up local vars in your context.
+    raise EnvironmentError('lookup_formal() not overloaded.')
+
+  def lookup_type(self, name):
+    # Overload this if you need to look up types (classes or interfaces) in
+    # your context.
+    raise EnvironmentError('lookup_type() not overloaded.')
+
+class EnvironmentError(Exception):
+  def __init__(self, msg):
+    self.msg = msg
