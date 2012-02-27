d110f23da5c4f50026d79cdf40554ffc4d9f969e
Andrew Song <song.exe@gmail.com>
2012-02-27 18:31:51 -0500

    Add method environment to the tree.

---
+++ name_resolution/method_env.py
@@ -0,0 +1,52 @@
+import env
+import parser.ast.ast_method as ast_method
+
+class MethodEnvironment(env.Environment):
+  '''A method environment that stores the formal parameters of the method.'''
+
+  def __init__(self, parent, ast):
+    super(MethodEnvironment, self).__init__(parent)
+
+    if type(ast) != ast_method.ASTMethod:
+      raise MethodEnvironmentError(
+          'Method environment was given non-ASTMethod node')
+
+    self.formals = {}
+    ast.environment = self
+
+    self.handle_ast(ast)
+
+  def handle_ast(self, ast):
+    '''Handle adding formal parameters from the AST to the environment'''
+    for p in ast.params:
+      self.add_formal(str(p.name), p)
+
+  def add_formal(self, name, ast):
+    '''Add a formal parameter to this environment'''
+    if name in self.formals:
+      raise MethodEnvironmentError(
+          'Found method with two paramaters of the same name: {0}'.format(name))
+
+    self.formals[name] = ast
+
+  def lookup_formal(self, name):
+    '''Look up a formal paramater in this environment'''
+    return self.formals.get(name)
+
+  def lookup_local(self, name):
+    # We don't want lookup_local to propogate up any further.
+    return None
+
+  # The rest of the lookup methods should just call the parent version.
+  def lookup_type(self, name):
+    return self.parent.lookup_type(name)
+
+  def lookup_field(self, name):
+    return self.parent.lookup_field(name)
+
+  def lookup_method(self, sig):
+    return self.parent.lookup_method(sig)
+
+
+class MethodEnvironmentError(env.EnvironmentError):
+  pass
