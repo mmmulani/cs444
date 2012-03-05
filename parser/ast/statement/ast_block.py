94ccbc6d85921c5cf44fe5efc6d87fdecd6c85a2
Mehdi Mulani <mmmulani@uwaterloo.ca>
2012-03-05 10:26:10 -0500

    Nest multiple variable declarations into blocks.

--- parser/ast/statement/ast_block.py
+++ parser/ast/statement/ast_block.py
@@ -2,6 +2,8 @@ import parser.ast.ast_node as ast_node
 import parser.ast.ast_variable_declaration as ast_variable_declaration
 import ast_statement
 
+from parser.ast.ast_node import ASTUtils
+
 class ASTBlock(ast_node.ASTNode):
   def __init__(self, tree):
     '''Creates an AST block node from a 'Block' TreeNode'''
@@ -13,6 +15,11 @@ class ASTBlock(ast_node.ASTNode):
     # This is set by the Environment module when the tree is complete.
     self.environment = None
 
+    # We use this hack to create ASTBlocks without a parse tree in
+    # from_statements.
+    if tree is None:
+      return
+
     if len(tree.children) == 2:
       return
 
@@ -23,25 +30,69 @@ class ASTBlock(ast_node.ASTNode):
       statements = statements.children[0]
     stmt_list.append(statements.children[0].children[0])
 
+    statements = []
     for stmt in stmt_list:
       if stmt.value == 'Statement':
-        self.children.append(ast_statement.ASTStatement.get_statement(stmt))
+        statements.append(ast_statement.ASTStatement.get_statement(stmt))
       elif stmt.value == 'LocalVariableDeclarationStatement':
-        self.children.append(ast_variable_declaration.ASTVariableDeclaration(
+        statements.append(ast_variable_declaration.ASTVariableDeclaration(
             stmt.children[0]))
       else:
         raise ASTBlockError('Block treenode has invalid children')
 
     # Reverse the statements so they're in the correct order.
-    self.children.reverse()
+    statements.reverse()
+
+    self.children = self.process_statements(statements)
+
+  @staticmethod
+  def from_statements(statements):
+    new_block = ASTBlock(None)
+    new_block.children = new_block.process_statements(statements)
+    return new_block
+
+  def process_statements(self, statements):
+    # Check to see if we have two VariableDeclarations in the statements. If we
+    # do, wrap the second VariableDeclaration and all statements after it in a
+    # new block. This makes determining if a variable has been used before its
+    # declaration much easier.
+
+    found_ix = -1
+    seen_declaration = False
+    for ix, statement in enumerate(statements):
+      if type(statement) == ast_variable_declaration.ASTVariableDeclaration:
+        if seen_declaration:
+          found_ix = ix
+          break
+        else:
+          seen_declaration = True
+
+    if found_ix == -1:
+      return statements
+
+    statements_before_decl = statements[:ix]
+    statements_with_decl = statements[ix:]
+
+    new_block = ASTBlock.from_statements(statements_with_decl)
+
+    statements_before_decl.append(new_block)
+
+    return statements_before_decl
 
   @property
   def statements(self):
     return self.children
 
-  def show(self, depth = 0):
+  def show(self, depth=0, show_name=False):
+    if show_name:
+      ASTUtils.println('ASTBlock:', depth)
+      depth = depth + 1
+
     for c in self.children:
-      c.show(depth)
+      if type(c) == ASTBlock:
+        c.show(depth, show_name=True)
+      else:
+        c.show(depth)
 
 class ASTBlockError(Exception):
   pass
