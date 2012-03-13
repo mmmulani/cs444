0ca3e07b80da4e5f56670d12bd717e4aeaa1b217
Andrew Song <song.exe@gmail.com>
2012-03-12 23:28:33 -0400

    Add the first parts of reachability checking.

---
+++ static_analysis/constraints.py
@@ -0,0 +1,60 @@
+import reachability
+
+# from reachability import check_block_or_statement
+
+'''
+  Each rule in this file should have the form:
+      def constraint_name(ast, i, o):
+  where:
+    1) ast is the ASTNode of the statement being checked
+    2) i is the value for IN for the previous statement
+    3) o is the value for OUT for the previous statement
+
+  Each constraint should return a tuple (i, o), where:
+    1) i is the IN value of the statement, and
+    2) o is the OUT value of the statement
+  Note that the returned IN value on a constraint is almost always
+  the OUT value of the previous statement (i.e. the OUT value passed in).
+'''
+
+# Constants defined for reachability analysis.
+MAYBE = 'maybe'
+NO = 'no'
+
+def var_decl(ast, i, o):
+  '''Reachability analysis on a variable declarator.'''
+  # Variable declarations aren't really statements, so we don't actually need
+  # to check anything here.  This constraint is just conveinient because
+  # variable declarations appear in ASTBlocks with other statements.
+
+
+  # If the previous statement finished executing, then we can start executing.
+  # By the "Any other statement" rule, out[L] = in[L] (= o, in this case)
+  return o, o 
+
+def return_statement(ast, i, o):
+  '''Reachabilty for a return statement.'''
+  # OUT of a return statement is always NO.
+  return o, NO
+
+def if_statement(ast, i, o):
+  '''Reachability for an IF statement.
+  L: if (E) S_1 else S_2
+  In[S_1] = In[L]
+  In[S_2] = In[L]
+  Out[L] = Out[S_1] or Out[S_2]'''
+
+  if_in, if_out = reachability.check_block_or_statement(
+      ast.if_statement, in_value=o)
+
+  if ast.else_statement is not None:
+    else_in, else_out = reachability.check_block_or_statement(
+        ast.else_statement, in_value=o)
+
+    # The out value is the OR between the if and else out values.
+    new_out = (MAYBE if if_out == MAYBE or else_out == MAYBE else NO)
+    return o, new_out
+
+  # If statement with no else:
+  #   Out[L] = Out[S_1] OR In[L] = In[L] = o
+  return o, o 
---
+++ static_analysis/reachability.py
@@ -0,0 +1,99 @@
+import constraints
+import parser.ast.ast_class as ast_class
+import parser.ast.ast_expression as ast_expression
+import parser.ast.ast_interface as ast_interface
+import parser.ast.ast_root as ast_root
+import parser.ast.ast_type as ast_type
+import parser.ast.ast_variable_declaration as ast_variable_declaration
+import parser.ast.statement.ast_block as ast_block
+import parser.ast.statement.ast_if as ast_if
+import parser.ast.statement.ast_for as ast_for
+import parser.ast.statement.ast_return as ast_return
+
+from constraints import MAYBE, NO
+
+statement_map = {
+  ast_if.ASTIf: constraints.if_statement,
+  ast_return.ASTReturn: constraints.return_statement,
+  ast_variable_declaration.ASTVariableDeclaration: constraints.var_decl
+}
+
+def check_reachability(ast):
+  '''Check the reachability of a file given its ASTRoot'''
+  root = ast
+  ast = ast.class_or_interface
+  if ast is None:
+    return
+
+  for m in ast.methods:
+    _check_method(m)
+
+  # No problems -- return here.
+  return
+
+def check_block_or_statement(ast, in_value):
+  '''Check the reachability of a block or statement.
+  This should be used in contexts where a block or a single statement could
+  appear, e.g. if statements, while loops, etc.'''
+  if isinstance(ast, ast_block.ASTBlock):
+    return _check_block(ast, in_value)
+  return _check_statement(ast, in_value, MAYBE)
+
+def _check_method(ast):
+  '''Check the reachability of a method'''
+  # Only check methods with bodies.
+  if ast.body is None:
+    return
+
+  in_value, out_value = _check_block(ast.body, in_value = MAYBE)
+
+  # For non-constructor, non-void methods, the OUT of the method must be
+  # NO, not MAYBE.
+  if not ast.is_constructor and ast.return_type != ast_type.ASTType.ASTVoid:
+    if out_value == MAYBE:
+      raise ReachabilityError('Method {0} has OUT = MAYBE'.format(ast.name))
+
+def _check_block(ast, in_value = MAYBE):
+  '''Check the reachability of a block'''
+  original_i = in_value
+  out_value = MAYBE
+
+  for statement in ast.statements:
+    if isinstance(statement, ast_expression.ASTExpression):
+      # Skip over any ASTExpressions.
+      pass
+    elif isinstance(statement, ast_block.ASTBlock):
+      in_value, out_value = _check_block(statement, in_value)
+    else:
+      in_value, out_value = _check_statement(statement, in_value, out_value)
+
+    # IN must be MAYBE for all statements
+    if in_value == NO:
+      raise ReachabilityError('Statement has IN = NO')
+
+  return original_i, out_value
+
+def _check_statement(ast, in_value, out_value):
+  '''Checks the reachability of a single statement.
+  This takes in the previous statement's in and out values.'''
+
+  # Check if we've memoized the result for this node already.
+  if ast.reachability != (None, None):
+    return ast.reachability
+
+  # Run the matching constraint on the statement.
+  # TODO(songandrew): Once they're all implemented, the IF should be removed.
+  # If there's a statement with no matching key, then we should throw an
+  # Exception as it means some other part of our code is broken.
+  if statement_map.has_key(type(ast)):
+    constraint = statement_map[type(ast)]
+    in_value, out_value = constraint(ast, in_value, out_value)
+    ast.reachability = (in_value, out_value)
+
+  # TODO(songandrew): Remove print.
+  print ast
+  return in_value, out_value
+
+class ReachabilityError(Exception):
+  def __init__(self, msg = ''):
+    self.msg = msg
