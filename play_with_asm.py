c6b585de52bd64bf10bc6b17b4e546b820747dd7
mmmulani <mmmulani@nettop20.student.cs.uwaterloo.ca>
2012-03-22 19:08:38 -0400

    Add helper scripts to test ASM.

---
+++ play_with_asm.py
@@ -0,0 +1,53 @@
+import scanner.scanner as scanner
+import parser.parser as parser
+import parser.ast.ast_node as ast_node
+import parser.ast.ast_root as ast_root
+import parser.ast.statement.ast_block as ast_block
+import weeder.weeder as weeder
+
+import code_gen.literal as literal
+import code_gen.binary as binary
+
+FILENAME = "j_play_with_asm.java"
+f = open(FILENAME)
+file_str = f.read()
+toks = scanner.TokenConverter.convert(scanner.Scanner.get_token_list(file_str))
+parse_tree = (parser.Parser()).parse(toks)
+(weeder.Weeder()).weed(parse_tree, FILENAME)
+ast = ast_root.ASTRoot(parse_tree)
+class_ast = ast.children[2]
+
+def get_blocks_from_class(class_ast):
+  blocks = []
+  for method in class_ast.children[1]:
+    blocks.extend(get_blocks_helper(method))
+  return blocks
+
+def get_blocks_helper(ast):
+  blocks = []
+  for x in ast.children:
+    if not isinstance(x, ast_node.ASTNode):
+      continue
+    elif isinstance(x, ast_block.ASTBlock):
+      blocks.append(x)
+    else:
+      blocks.extend(get_blocks_helper(x))
+  return blocks
+
+def flatten_asm(lst):
+  strs = []
+  for x in lst:
+    if type(x) == type(''):
+      strs.append(x)
+    else:
+      strs.extend(flatten_asm(x))
+  return strs
+
+def print_asm(lst):
+  print '\n'.join(flatten_asm(lst))
+
+blocks = get_blocks_from_class(class_ast)
+asm = blocks[1].children[0].children[0].c_gen_code()
+# print_asm(asm)
+# print_asm(literal.create_int())
+# print_asm(binary.add_int())
