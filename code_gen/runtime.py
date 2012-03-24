1fa6e23820a404ba02f78a98fb0557ff6523d8d7
Andrew Song <song.exe@gmail.com>
2012-03-24 14:18:15 -0400

    Add a comment to runtime.py

--- code_gen/runtime.py
+++ code_gen/runtime.py
@@ -1,6 +1,9 @@
+# Use the file to store the runtime.s names.
 NAMES = [
   '__malloc',
   '__debexit',
   '__exception',
   'NATIVEjava.io.OutputStream.nativeWrite'
 ]
+
+# THIS FILE INTENTIONALLY LEFT BLANK.
