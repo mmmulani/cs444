d054ed560088a8baf6d28a4e407371930a892316
Andrew Song <song.exe@gmail.com>
2012-03-24 14:47:43 -0400

    Clean up play_with_asm

--- play_with_asm.py
+++ play_with_asm.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import scanner.scanner as scanner
 import parser.parser as parser
 import parser.ast.ast_node as ast_node
@@ -8,14 +9,30 @@ import weeder.weeder as weeder
 import code_gen.literal as literal
 import code_gen.binary as binary
 
+# This script will read this file in the working dir.  It should have the form:
+# public class X {
+#   public X() { }
+#   public static int test() {
+#     return <some expr here>;
+#   }
+# }
 FILENAME = "j_play_with_asm.java"
-f = open(FILENAME)
-file_str = f.read()
-toks = scanner.TokenConverter.convert(scanner.Scanner.get_token_list(file_str))
-parse_tree = (parser.Parser()).parse(toks)
-(weeder.Weeder()).weed(parse_tree, FILENAME)
-ast = ast_root.ASTRoot(parse_tree)
-class_ast = ast.children[2]
+
+def main():
+  f = open(FILENAME)
+  file_str = f.read()
+  toks = scanner.TokenConverter.convert(
+      scanner.Scanner.get_token_list(file_str))
+  parse_tree = (parser.Parser()).parse(toks)
+  (weeder.Weeder()).weed(parse_tree, FILENAME)
+  ast = ast_root.ASTRoot(parse_tree)
+  class_ast = ast.children[2]
+
+  # Get the assembly of the return statement.
+  blocks = get_blocks_from_class(class_ast)
+  asm = blocks[1].children[0].children[0].c_gen_code()
+  print_asm(asm)
+
 
 def get_blocks_from_class(class_ast):
   blocks = []
@@ -46,8 +63,5 @@ def flatten_asm(lst):
 def print_asm(lst):
   print '\n'.join(flatten_asm(lst))
 
-blocks = get_blocks_from_class(class_ast)
-asm = blocks[1].children[0].children[0].c_gen_code()
-# print_asm(asm)
-# print_asm(literal.create_int())
-# print_asm(binary.add_int())
+if __name__ == '__main__':
+  main()
