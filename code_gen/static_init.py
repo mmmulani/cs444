74255d8a82ee5c96ef43210d679c8b1d29e453fe
Mehdi Mulani <mmmulani@uwaterloo.ca>
2012-04-01 17:13:50 -0400

    Set default values for static fields before initializing.

--- code_gen/static_init.py
+++ code_gen/static_init.py
@@ -1,18 +1,33 @@
 import asm.object
+import asm.common as common
 
 from manager import CodeGenManager
 
 def initialize_static_fields():
   '''Initializes static fields'''
   ret = []
+  # First set default values.
   for t, f in CodeGenManager.static_inits:
+    init = asm.object.create_default_value(f)
+    code = [
+      '; --- Default init for {0}.{1}'.format(t.canonical_name, f.identifier),
+      init,
+      common.set_static_field(f, 'eax'),
+      '',
+    ]
+    ret.append(code)
+
+  # Next we set all fields that have an explicit initialization expression.
+  for t, f in CodeGenManager.static_inits:
+    if f.expression is None:
+      continue
+
     init = asm.object.create_starting_value(f)
     code = [
       '; --- INIT for {0}.{1} ---'.format(t.canonical_name, f.identifier),
       init,
       '; --- END INIT ---',
-      'mov dword ebx, [{0} + {1}]'.format(t.c_cit_label, f.c_offset),
-      'mov dword [ebx], eax',
+      common.set_static_field(f, 'eax'),
       ''
     ]
     ret.append(code)
