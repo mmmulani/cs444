d9f99c58273d21d430671ad3de9c242aaf432661
Andrew Song <song.exe@gmail.com>
2012-04-01 02:01:37 -0400

    Init static vars using Mehdi's code!

--- code_gen/static_init.py
+++ code_gen/static_init.py
@@ -1,14 +1,18 @@
+import asm.object
+
 from manager import CodeGenManager
 
 def initialize_static_fields():
   '''Initializes static fields'''
   ret = []
   for t, f in CodeGenManager.static_inits:
-    label = '; Init for {0}.{1}'.format(t.canonical_name, f.identifier)
+    init = asm.object.create_starting_value(f)
     code = [
-      label,
+      '; --- INIT for {0}.{1} ---'.format(t.canonical_name, f.identifier),
+      init,
+      '; --- END INIT ---',
       'mov dword ebx, [{0} + {1}]'.format(t.c_cit_label, f.c_offset),
-      'mov dword [ebx], 0x0',
+      'mov dword [ebx], eax',
       ''
     ]
     ret.append(code)
